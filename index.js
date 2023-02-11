0d7e8e0148a854eb8342df680effbe55ff8c8261
Vu <vunguyentien10@gmail.com>
2023-02-12 01:11:18 +0700

    fix cors

--- index.js
+++ index.js
@@ -10,7 +10,27 @@ const messageRoute = require("./routes/messageRoute");
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://chat-f2cg.onrender.com"]
+}));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://chat-f2cg.onrender.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(express.json());
 
 app.use("/v1/user", userRoute);
